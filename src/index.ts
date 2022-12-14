817c172d1a60f11ab4bdaec030b3c84742dc38c3
TEDIORELEE <tediorelee@gmail.com>
2022-12-14 15:17:06 +0000

    feat: switch to new api

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,47 @@ export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
-const apiEndpointPrefix = 'https://api.bhawanigarg.com/social/instagram/?url=';
-
-export const GRAPH_SIDECAR = 'GraphSidecar';
-export const GRAPH_IMAGE = 'GraphImage';
-export const GRAPH_VIDEO = 'GraphVideo';
+const apiEndpointPrefix = 'http://api.rcuts.com/2022/api.php';
 
 export function apply(ctx: Context) {
-  function fetchImageFromAPI(url: string) {
-    return ctx.http.get(apiEndpointPrefix + url);
+  function getParsedUrlFromAPI (url: string) {
+    const headers= {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    };
+    const formData = {
+      text: url,
+      token: '',
+      clipboard: '',
+      pt: 'instagram'
+    };
+    return ctx.http.axios(apiEndpointPrefix, {
+      method: 'POST',
+      data: formData,
+      headers
+    });
+  };
+
+  function getHtmlData (url: string) {
+    return ctx.http.get(url)
+  };
+
+
+  function fetchImageFromAPI(url: string, result: any) {
+    const headers= {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    };
+    const formData = {
+      text: url,
+      token: '',
+      clipboard: '',
+      pt: 'instagram',
+      html: result
+    };
+    return ctx.http.axios(apiEndpointPrefix, {
+      method: 'POST',
+      data: formData,
+      headers
+    });
   };
 
   ctx.middleware(async (session, next) => {
@@ -22,36 +54,19 @@ export function apply(ctx: Context) {
 
     if (!session.content.includes('instagram.com')) return next()
 
-    const result = await fetchImageFromAPI(session.content);
-
     try {
-      if (result.status === 0) return 'Invalid URL or ID!';
-      const { graphql: { shortcode_media } } = result;
-
-      // only one image in instagram post
-      if (shortcode_media.__typename === GRAPH_IMAGE) {
-        const img = await ctx.http.get<ArrayBuffer>(shortcode_media.display_resources[0].src, {
+      const response = await getParsedUrlFromAPI(session.content);
+      const formattedUrl = response.data.url;
+      const htmlData = await getHtmlData(formattedUrl)
+      const imgResponse = await fetchImageFromAPI(session.content, htmlData)
+      const imgList = imgResponse.data.list;
+      imgList.forEach(async img => {
+        const parsedImg = await ctx.http.get<ArrayBuffer>(img, {
           responseType: 'arraybuffer',
-        })
-        await session.sendQueued(segment.image(img))
-      } else if (shortcode_media.__typename === GRAPH_VIDEO) {
-        // only one video in instagram post
-        await session.sendQueued(segment.video(shortcode_media.video_url))
-      } else {
-        // contains both video and images or multiple images or videos
-        const { edges } = shortcode_media.edge_sidecar_to_children;
-        edges.forEach(async item => {
-          if (item.node.is_video) {
-            await session.sendQueued(segment.video(item.node.video_url))
-          } else {
-            const length = item.node.display_resources.length;
-            const img = await ctx.http.get<ArrayBuffer>(item.node.display_resources[length - 1].src, {
-              responseType: 'arraybuffer',
-            });
-            await session.sendQueued(segment.image(img))
-          }
         });
-      }
+        return session.sendQueued(`${segment.image(parsedImg)}`)
+      });
+
     } catch(err) {
       console.log(err);
       return `错误！${err}`;
